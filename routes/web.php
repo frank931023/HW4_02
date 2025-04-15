950561ca04431eaec9ccf18885fe1782bae8595a
ThanatosJun <ThanatosJun@gmail.com>
2025-04-15 16:54:44 +0800

    fix session that in original user table  must be to herd open

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\MemberController;
+use Illuminate\Support\Facades\Route;
 
-// 貼文路由
-Route::resource('posts', PostController::class);
-
-// 留言路由
-Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
-
-// 會員路由（如果有的話）
-Route::get('members/{member}', [MemberController::class, 'show'])->name('members.show');
+Route::get('/', function () {
+    return view('home');
+});
