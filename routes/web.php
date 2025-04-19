318d369f3f7c8c07cc8e77001d88f8b689e0fe54
Hsiao-Chi Hsieh <kida0978@gmail.com>
2025-04-19 19:46:26 +0800

    Fix: Adjust web.php for test and merge

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PageController;
 
+// 預設首頁顯示討論版列表 (測試用)
+Route::get('/', [PageController::class, 'pages'])->name('home');
+
+// Page 討論版列表頁面（merge用）
 Route::get('/pages', [PageController::class, 'pages'])->name('pages.pages');
+
+// Post 討論區列表頁面
 Route::get('/pages/{id}', [PageController::class, 'posts'])->name('pages.posts');
